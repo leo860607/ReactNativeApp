53c361304b80e004b2e1e58db67c5950fa0740e4
Draculelin <leo860607@gmail.com>
2022-02-24 15:34:50 +0800

    完成 : firebase 取得首頁圖示資料

--- src/feature/DataPage.tsx
+++ src/feature/DataPage.tsx
@@ -2,7 +2,7 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import React, { useRef, useState } from "react";
 import { View, Text, Image, StyleSheet, Button } from "react-native";
 import localStorage from "../shared/service/localStorage";
-
+import firestore from "@react-native-firebase/firestore"
 
 
 const styles = StyleSheet.create({
@@ -25,6 +25,16 @@ const styles = StyleSheet.create({
 });
 
 
+async function getFIreData(){
+    const animesCollection = firestore().collection('newsInfo')
+    const allAnimeDocument = animesCollection.doc('AllNews')
+    console.log("TT");
+    console.log(await allAnimeDocument.get())
+    allAnimeDocument.onSnapshot(documentSnapshot => {
+        console.log('User data: ', documentSnapshot.data());
+    });
+}
+
 
 
 const DataPage = (routeParam: { route: any }) => {
@@ -37,12 +47,9 @@ const DataPage = (routeParam: { route: any }) => {
         localStorage.setdata("Btn", { data: "IU forever" })
     }
 
-    const onMessage = (data: any) => {
-        //Prints out data that was passed.
-        console.log("TESTmessage");
-        console.log(data);
-    }
-    const webViewRef = useRef(null);
+
+
+
 
     return (
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
@@ -51,6 +58,10 @@ const DataPage = (routeParam: { route: any }) => {
             </View>
             <View style={{ width: 300, marginBottom: 10 }}>
                 <Button title="取值" onPress={getData} />
+                <Text>{tmp}</Text>
+            </View>
+            <View style={{ width: 300, marginBottom: 10 }}>
+                <Button title="取Fire值" onPress={getFIreData} />
                 <Text>{tmp}</Text>
             </View>
         </View>
--- src/feature/HomePage.tsx
+++ src/feature/HomePage.tsx
@@ -1,9 +1,9 @@
 import { useNavigation } from "@react-navigation/native";
-import React, { cloneElement, Fragment } from "react";
+import React, { cloneElement, Fragment, useState } from "react";
 import { Button, Image, SafeAreaView, ScrollView, StatusBar, StyleSheet, Text, useColorScheme, View } from "react-native";
 import { Colors, DebugInstructions, Header, LearnMoreLinks, ReloadInstructions } from "react-native/Libraries/NewAppScreen";
 import { newsInfo } from "../shared/model/news";
-
+import firestore from "@react-native-firebase/firestore"
 
 
 
@@ -36,14 +36,27 @@ const Section: React.FC<{
   );
 };
 
+function getFIreData(): any {
+  const animesCollection = firestore().collection('newsInfo')
+  const allAnimeDocument = animesCollection.doc('AllNews')
+  allAnimeDocument.onSnapshot(documentSnapshot => {
+    // console.log('User data: ', documentSnapshot.data());
+    console.log("GG")
+    // setValue(documentSnapshot.data()!.News);
+    return documentSnapshot.data()!.News
+  });
+}
+
 const HomePage = () => {
-  const news: newsInfo[] = [{
+  const [tmp, setValue] = useState([]);
+  // let news: newsInfo[] = [{
+  let news: any = [{
     id: "1",
     title: "李知恩",
     imgLink: "https://i.scdn.co/image/ab6761610000e5eb006ff3c0136a71bfb9928d34",
     likes: 0,
     isFavorite: true,
-    webUrl:"http://10.0.2.2:5000/"
+    webUrl: "http://10.0.2.2:5000/"
     // "https://open.spotify.com/artist/3HqSLMAZ3g3d5poNaI7GOU"
   },
   {
@@ -52,32 +65,44 @@ const HomePage = () => {
     imgLink: "https://i.discogs.com/T0yZ16BMznGeiyv4Q2eMgOoRlUmM_iRLBECCDXANPYU/rs:fit/g:sm/q:40/h:300/w:300/czM6Ly9kaXNjb2dz/LWltYWdlcy9BLTIy/MjYzMDYtMTU5NzMz/MjM5Mi03MzMwLmpw/ZWc.jpeg",
     likes: 0,
     isFavorite: true,
-    webUrl:"https://script.google.com/macros/s/AKfycbx2uNq0s6tCF1WfXMA2fFJf5fFQn1I6p9Ggmn3RHc0SEZ5P-MN99lzB2K23Tkh__sWWGA/exec",
+    webUrl: "https://script.google.com/macros/s/AKfycbx2uNq0s6tCF1WfXMA2fFJf5fFQn1I6p9Ggmn3RHc0SEZ5P-MN99lzB2K23Tkh__sWWGA/exec",
     // https://script.google.com/macros/s/AKfycbx2uNq0s6tCF1WfXMA2fFJf5fFQn1I6p9Ggmn3RHc0SEZ5P-MN99lzB2K23Tkh__sWWGA/exec
   }
   ]
   const navigation = useNavigation();
+  const animesCollection = firestore().collection('newsInfo')
+  const allAnimeDocument = animesCollection.doc('AllNews')
+  //用unsubscribe接起來避免listening (一直重複打)
+  const unsubscribe = allAnimeDocument.onSnapshot(documentSnapshot => {
+    console.log('User data: ', documentSnapshot.data());
+    // console.log("GG")
+    setValue(documentSnapshot.data()!.News)
+  });
+  unsubscribe();
+
   return (
     <SafeAreaView>
       <ScrollView >
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }} >
           <View style={styles.titleContent}>
             <Text style={styles.title}>最新消息</Text>
           </View>
-          {news.map(element => {
-            const url = element.imgLink;
-            return <View key={element.id} style={styles.card}>
-              <Image source={{ uri: url }}
-                style={{ width: 300, height: 300 }}
+          {
+            tmp ? tmp.map((element: any) => {
+              const url = element.imgLink;
+              return <View key={element.id} style={styles.card}>
+                <Image source={{ uri: url }}
+                  style={{ width: 300, height: 300 }}
 
-              ></Image>
-              <Button
-                title={element.title}
-                color="#f194ff"
-                onPress={() => navigation.navigate("detail" as never, element as never)}
-              />
-            </View>
-          })}
+                ></Image>
+                <Button
+                  title={element.title}
+                  color="#f194ff"
+                  onPress={() => navigation.navigate("detail" as never, element as never)}
+                />
+              </View>
+            }) : []
+          }
         </View>
       </ScrollView>
     </SafeAreaView>
@@ -155,3 +180,5 @@ const styles = StyleSheet.create({
     width: "100%",
   },
 });
+
+
